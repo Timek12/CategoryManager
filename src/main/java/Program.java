efe9684352a20d854a4e43b9fc5461900275d295
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-13 13:03:00 +0100

    Add shopping list service and display menu in while loop

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,8 +1,15 @@
+import Models.Category;
 import Services.CategoryService;
+import Services.ShoppingListManagementService;
 
 public class Program {
     public static void main(String args[]) {
         CategoryService categoryService = new CategoryService("src/main/java/products.txt");
+        ShoppingListManagementService shoppingListManagementService = new ShoppingListManagementService();
 
+        while(true) {
+            shoppingListManagementService.displayMenu();
+
+        }
     }
 }
