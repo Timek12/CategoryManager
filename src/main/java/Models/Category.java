130fea1485b411cae426038326e8bff013148838
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-13 18:25:15 +0100

    Add constructors for category model

--- src/main/java/Models/Category.java
+++ src/main/java/Models/Category.java
@@ -1,8 +1,16 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
+    public Category(String name) {
+        this.name = name;
+        this.products = new ArrayList<>();
+    }
+    public Category() {
+        this.products = new ArrayList<>();
+    }
     public String name;
     public List<String> products;
 }
