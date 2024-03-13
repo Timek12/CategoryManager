02291c88207966ddbbc4ee83e175a7a902ace5cc
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-13 18:25:00 +0100

    Rename ShoppingListManagementService to ShoppingListService, define method for adding product to category

--- src/main/java/Services/ShoppingListService.java
+++ src/main/java/Services/ShoppingListService.java
@@ -5,8 +5,9 @@ import Models.Category;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShoppingListManagementService {
+public class ShoppingListService {
     public List<Category> categories = new ArrayList<>();
+
     public void displayMenu() {
         System.out.println("1. Add product to shopping list.");
         System.out.println("2. Display all products from shopping list.");
@@ -17,7 +18,12 @@ public class ShoppingListManagementService {
         System.out.println("7. Save shopping list to file.");
         System.out.println("8. Exit.");
     }
+
     public void displayAllProducts() {
+        if (this.categories.isEmpty()) {
+            System.out.println("No products in shopping list.");
+            return;
+        }
         for (Category category : this.categories) {
             System.out.println("[" + category.name + "]");
             for (String product : category.products) {
@@ -31,6 +37,10 @@ public class ShoppingListManagementService {
         for (Category category : this.categories) {
             if (category.name.equals(categoryName)) {
                 System.out.println("[" + category.name + "]");
+                if (category.products.isEmpty()) {
+                    System.out.println("No products in this category.");
+                    return;
+                }
                 for (String product : category.products) {
                     System.out.println(" - " + product);
                 }
@@ -39,5 +49,29 @@ public class ShoppingListManagementService {
             }
         }
         System.out.println("Category not found");
+    }
+
+    public void addProductToCategory(String categoryName, String productName) {
+        boolean isCategoryFound = false;
+        for (Category category : this.categories) {
+            if (category.name.equals(categoryName)) {
+                category.products.add(productName);
+                isCategoryFound = true;
+                break;
+            }
+        }
+
+        if (isCategoryFound) {
+            return;
+        }
+
+        this.categories.add(new Category(categoryName));
+
+        for (Category category : this.categories) {
+            if (category.name.equals(categoryName)) {
+                category.products.add(productName);
+                return;
+            }
+        }
     }
 }
